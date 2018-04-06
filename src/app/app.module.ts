74c028c9fc74bd1360d4f5b4da943c8e686eb04b
Dedy Sutanto <dedy.sutanto@proit.co.id>
2018-04-06 17:33:08 +0700

    Make it Work with component -> <router-outlet> is the key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from './modules/material.module';
+import { AppRoutingModule } from './modules/approuting.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
