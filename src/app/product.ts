65c224f4a149d9cd964dfd888bc30cf2168550e7
Dedy Sutanto <dedy.sutanto@proit.co.id>
2018-04-09 08:31:07 +0700

    Product models is change to complete fields

--- src/app/product.ts
+++ src/app/product.ts
@@ -8,6 +8,10 @@ export class Product {
     isTimerRunning: boolean;
     isClicked: boolean;
     uuid: string;
+    start_time: string;
+    end_time: string;
+    displayed_item: number;
+    wasted_item: number;
 
     constructor(value: Object = {}) {
         Object.assign(this, value);
